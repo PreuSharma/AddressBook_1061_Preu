c88c330b41df2992fc80b5452f7277cb5ab9a9a6
Preu Sharma <preu1061.be21@chitkara.edu.in>
2025-01-27 13:37:47 +0530

    [Preu] Add: adding the addressbook uc3

--- ucc1.js
+++ ucc1.js
@@ -39,6 +39,52 @@ var AddressBook = /** @class */ (function () {
             this.contacts.forEach(function (contact) { return console.log(contact.toString()); });
         }
     };
+    // Method to find a contact by their first or last name
+    AddressBook.prototype.findContact = function (name) {
+        return this.contacts.find(function (contact) {
+            return contact.firstName.toLowerCase() === name.toLowerCase() ||
+                contact.lastName.toLowerCase() === name.toLowerCase();
+        });
+    };
+    // Method to edit a contact's details
+    AddressBook.prototype.editContact = function (name) {
+        var contact = this.findContact(name);
+        if (contact) {
+            console.log("Editing contact: ".concat(contact.toString()));
+            // Ask user for new details and update only the ones they provide
+            readline.question("Enter updated address (or press Enter to skip): ", function (address) {
+                readline.question("Enter updated city (or press Enter to skip): ", function (city) {
+                    readline.question("Enter updated state (or press Enter to skip): ", function (state) {
+                        readline.question("Enter updated zip (or press Enter to skip): ", function (zip) {
+                            readline.question("Enter updated phone number (or press Enter to skip): ", function (phone) {
+                                readline.question("Enter updated email (or press Enter to skip): ", function (email) {
+                                    // Update the contact only with provided values
+                                    if (address)
+                                        contact.address = address;
+                                    if (city)
+                                        contact.city = city;
+                                    if (state)
+                                        contact.state = state;
+                                    if (zip)
+                                        contact.zip = zip;
+                                    if (phone)
+                                        contact.phoneNumber = phone;
+                                    if (email)
+                                        contact.email = email;
+                                    console.log("Contact updated successfully!");
+                                    mainMenu(); // Return to the main menu
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        }
+        else {
+            console.log("Contact not found.");
+            mainMenu(); // Return to the main menu if contact is not found
+        }
+    };
     return AddressBook;
 }());
 // Create an instance of AddressBook
@@ -65,12 +111,19 @@ function addNewContact() {
         });
     });
 }
-// Main menu to guide user to create or view contacts
+// Function to edit an existing contact
+function editExistingContact() {
+    readline.question("Enter the first or last name of the contact to edit: ", function (name) {
+        addressBook.editContact(name); // Call the edit method in AddressBook
+    });
+}
+// Main menu to guide user to create, edit or view contacts
 function mainMenu() {
     console.log("\nWelcome to the Address Book!");
     console.log("1. Add a new contact");
     console.log("2. View all contacts");
-    console.log("3. Exit");
+    console.log("3. Edit an existing contact");
+    console.log("4. Exit");
     readline.question("Choose an option: ", function (choice) {
         switch (choice) {
             case "1":
@@ -81,6 +134,9 @@ function mainMenu() {
                 mainMenu(); // Return to the main menu after displaying
                 break;
             case "3":
+                editExistingContact(); // Edit an existing contact
+                break;
+            case "4":
                 console.log("Exiting Address Book. Goodbye!");
                 readline.close();
                 break;
--- ucc1.ts
+++ ucc1.ts
@@ -53,6 +53,48 @@ class AddressBook {
             this.contacts.forEach(contact => console.log(contact.toString()));
         }
     }
+
+    // Method to find a contact by their first or last name
+    public findContact(name: string): IContact | undefined {
+        return this.contacts.find(contact => 
+            contact.firstName.toLowerCase() === name.toLowerCase() || 
+            contact.lastName.toLowerCase() === name.toLowerCase());
+    }
+
+    // Method to edit a contact's details
+    public editContact(name: string): void {
+        const contact = this.findContact(name);
+        if (contact) {
+            console.log(`Editing contact: ${contact.toString()}`);
+            
+            // Ask user for new details and update only the ones they provide
+            readline.question("Enter updated address (or press Enter to skip): ", (address: string) => {
+                readline.question("Enter updated city (or press Enter to skip): ", (city: string) => {
+                    readline.question("Enter updated state (or press Enter to skip): ", (state: string) => {
+                        readline.question("Enter updated zip (or press Enter to skip): ", (zip: string) => {
+                            readline.question("Enter updated phone number (or press Enter to skip): ", (phone: string) => {
+                                readline.question("Enter updated email (or press Enter to skip): ", (email: string) => {
+                                    // Update the contact only with provided values
+                                    if (address) contact.address = address;
+                                    if (city) contact.city = city;
+                                    if (state) contact.state = state;
+                                    if (zip) contact.zip = zip;
+                                    if (phone) contact.phoneNumber = phone;
+                                    if (email) contact.email = email;
+                                    
+                                    console.log("Contact updated successfully!");
+                                    mainMenu(); // Return to the main menu
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        } else {
+            console.log("Contact not found.");
+            mainMenu(); // Return to the main menu if contact is not found
+        }
+    }
 }
 
 // Create an instance of AddressBook
@@ -81,12 +123,20 @@ function addNewContact() {
     });
 }
 
-// Main menu to guide user to create or view contacts
+// Function to edit an existing contact
+function editExistingContact() {
+    readline.question("Enter the first or last name of the contact to edit: ", (name: string) => {
+        addressBook.editContact(name); // Call the edit method in AddressBook
+    });
+}
+
+// Main menu to guide user to create, edit or view contacts
 function mainMenu() {
     console.log("\nWelcome to the Address Book!");
     console.log("1. Add a new contact");
     console.log("2. View all contacts");
-    console.log("3. Exit");
+    console.log("3. Edit an existing contact");
+    console.log("4. Exit");
 
     readline.question("Choose an option: ", (choice: string) => {
         switch (choice) {
@@ -98,6 +148,9 @@ function mainMenu() {
                 mainMenu(); // Return to the main menu after displaying
                 break;
             case "3":
+                editExistingContact(); // Edit an existing contact
+                break;
+            case "4":
                 console.log("Exiting Address Book. Goodbye!");
                 readline.close();
                 break;
